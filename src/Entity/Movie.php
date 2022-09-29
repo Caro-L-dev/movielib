f6533ac879b973424edb8106e267c9eef8d88987
Caroline <liardcar@gmail.com>
2022-09-29 11:35:03 +0200

    feat: make entities + migration

---
+++ src/Entity/Movie.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $realisateur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $actor = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $releasedDate = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $poster = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getRealisateur(): ?string
+    {
+        return $this->realisateur;
+    }
+
+    public function setRealisateur(string $realisateur): self
+    {
+        $this->realisateur = $realisateur;
+
+        return $this;
+    }
+
+    public function getActor(): ?string
+    {
+        return $this->actor;
+    }
+
+    public function setActor(string $actor): self
+    {
+        $this->actor = $actor;
+
+        return $this;
+    }
+
+    public function getReleasedDate(): ?\DateTimeInterface
+    {
+        return $this->releasedDate;
+    }
+
+    public function setReleasedDate(\DateTimeInterface $releasedDate): self
+    {
+        $this->releasedDate = $releasedDate;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(?string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
